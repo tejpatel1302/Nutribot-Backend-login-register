fa93f21a875f5c7e277080f78f696cf46f015374
tejpatel1302 <tgp02tp@gmail.com>
2025-06-24 11:15:03 +0530

    vercel ready 2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-	origin: 'http://localhost:3000',
+	origin: 'https://nutribot-nine.vercel.app/',
 	methods: 'GET,POST,PUT,DELETE',
 	allowedHeaders: ['Content-Type', 'Authorization'],
   }));
